5bfc39849ad2eb5dce1c5582924cf6b7299d3c1c
adams-dylan <adams.dylan.chs@gmail.com>
2020-03-15 11:49:39 -0400

    Update test1.c

--- test1.c
+++ test1.c
@@ -16,19 +16,15 @@ int main()
 {
 	//srand(time(NULL));
 	int small = 0, big = 0;
-	for (int i = 0; i < 1000000; i++)
 	{
 		double j = get_rand();
-		if (j < 0.5)
 		{
 			small ++;
-		}
 		else if (j > 0.5)
 		{
 			big ++;
 		}
 	}
-	
 	printf("small= %d, big = %d\n", small, big);
 	
 	return 0;
