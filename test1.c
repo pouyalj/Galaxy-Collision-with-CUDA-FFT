eaeb4f143c967a87d63c2bc5d14adcf0b491d94b
adams-dylan <adams.dylan.chs@gmail.com>
2020-04-10 17:47:30 -0400

    Update test1.c
    updated test1

--- test1.c
+++ test1.c
@@ -1,31 +1,5 @@
-#include <stdio.h>
-#include <math.h>
-#include<time.h>
-#include<stdlib.h>
-#include "dislin.h"
-
-double get_rand(void)
-{
-	srand(time(NULL));
-	//This fuction will be used to generat random doubles between 0 and 1.
-	int num = rand() % 10001;
-	return (double)num * 0.0001;
-}
-
 int main()
 {
-	//srand(time(NULL));
-	int small = 0, big = 0;
-	{
-		double j = get_rand();
-		{
-			small ++;
-		else if (j > 0.5)
-		{
-			big ++;
-		}
-	}
-	printf("small= %d, big = %d\n", small, big);
-	
+	printf("This is a test\n");
 	return 0;
 }
